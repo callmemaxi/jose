10b4f82423f5315cd6f20252734b19c68a004167
Manjunatha Poojari <35322943+callmemaxi@users.noreply.github.com>
2024-04-25 17:29:26 +0530

    Update check_iv_length.ts
    Disable IV Length Check

--- src/lib/check_iv_length.ts
+++ src/lib/check_iv_length.ts
@@ -2,9 +2,9 @@ import { JWEInvalid } from '../util/errors.js'
 import { bitLength } from './iv.js'
 
 const checkIvLength = (enc: string, iv: Uint8Array) => {
-  if (iv.length << 3 !== bitLength(enc)) {
-    throw new JWEInvalid('Invalid Initialization Vector length')
-  }
+  // if (iv.length << 3 !== bitLength(enc)) {
+  //   throw new JWEInvalid('Invalid Initialization Vector length')
+  // }
 }
 
 export default checkIvLength
